320bb8effdfe6f8c3747653d1bed29d8b4e118a7
insua <insua@sohu.com>
2019-03-17 17:46:36 +0800

    First Blood

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('商城首页'),
       ),
     );
   }
+
+  void getHttp() async {
+    try {
+      Response response;
+      var data = {'name': '张翠山'};
+      response = await Dio().get(
+        "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=殷素素",
+      );
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
+  }
+
 }
